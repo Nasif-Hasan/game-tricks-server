3c83040d3dd08a4039ba388bf69c8195e1bca4bf
Nasif-Hasan <nasifhasan35@gmail.com>
2022-10-28 02:40:24 +0600

    api removed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.port || 5000;
 app.use(cors())
 
 const courses = require('./data/courses.json');
-const tutorial = require('./data/tutorial.json');
+
 
 app.get('/', (req, res) => {
     res.send('api running')
@@ -16,16 +16,12 @@ app.get('/game-courses', (req, res) => {
     res.send(courses)
 });
 
-// app.get('/category/:id', (req, res) => {
-//     const id = req.params.id
-//     const category = tutorial.filter(t => t.category === id)
-//     res.send(category)
-// })
 
-app.get('/game/:id', (req, res) => {
+app.get('/game-courses/:id', (req, res) => {
     const id = req.params.id
-    const selectedTutorial = tutorial.find(t => t.id === id)
-    res.send(selectedTutorial)
+    const selectedCourse = courses.find(t => t.id === id)
+    res.send(selectedCourse)
+    // console.log(selectedCourse);
 
 })
 
