4de3c3dc7ec82e09f07451fdefb5e943f2701f37
Nasif-Hasan <nasifhasan35@gmail.com>
2022-10-27 23:45:38 +0600

    data set

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.port || 5000;
 
 app.use(cors())
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const tutorial = require('./data/tutorial.json');
 
 app.get('/', (req, res) => {
     res.send('api running')
@@ -13,6 +14,19 @@ app.get('/', (req, res) => {
 
 app.get('/game-courses', (req, res) => {
     res.send(courses)
+});
+
+// app.get('/category/:id', (req, res) => {
+//     const id = req.params.id
+//     const category = tutorial.filter(t => t.category === id)
+//     res.send(category)
+// })
+
+app.get('/game/:id', (req, res) => {
+    const id = req.params.id
+    const selectedTutorial = tutorial.find(t => t.id === id)
+    res.send(selectedTutorial)
+
 })
 
 app.listen(port, () => {
